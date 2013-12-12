017bfb1cfc09958bad91884c9973ff170e5f5676
efelton <eoghan.felton@gmail.com>
2013-12-12 20:10:16 +0000

    Added list of implemented features and issues

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 
         <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
           <div class="list-group">
-            <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item active">Link</a>
+            <a href="features.html" class="list-group-item">Features</a>
             <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
             <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
             <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
